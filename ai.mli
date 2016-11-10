ffd26810d2e189abeab6137e27ab3a299e711a52
Nathaniel Kaplan <nak74@cornell.edu>
2016-11-09 21:47:50 -0500

    ok actually done with mli except for graphics now

--- ai.mli
+++ ai.mli
@@ -1,5 +1,5 @@
 open Card
 
 
-(** [determine move s returns the move that the ai should make this turn*)
+(** [determine move s] returns the move that the ai should make this turn*)
 val determine_move : state -> move
--- play.mli
+++ play.mli
@@ -1,3 +1,5 @@
+open Card
+
 (*Initilizes the first state, setting the first player and setting up the board*)
 val init_state : player -> state
 
@@ -15,10 +17,14 @@ val take_two_gems : player -> color -> state option
 (** [reserve_card p c] returns the state after [p] buys [c] *)
 val reserve_card : player -> card -> state
 
+(** [check_nobles p nobles] returns a list of nobles that a player is eligible
+* to take, or [None] if the player cannot take any*)
+val check_nobles : player -> noble list -> noble list option
+
 (** [end game p turns ends the game when player [p] reaches 15 points, with [turns]
 * turns remaining*)
 val end_game : player -> int -> state
 
 (** [play s m] is the main function that plays the game, and returns the state 
-after the current player makes move [m]*)
+* after the current player makes move [m] *)
 val play : state -> move -> state
