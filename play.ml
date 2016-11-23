054cac358828d748485b74cb0be5a86ca6164bc3
Nathaniel Kaplan <nak74@cornell.edu>
2016-11-23 15:09:08 -0500

    started constructing decks

--- play.ml
+++ play.ml
@@ -1,6 +1,91 @@
 open Card
 open Ai
-open Graphic
+
+let make_card color points gem_cost = 
+	{color = color;
+	points = points;
+	gem_cost = gem_cost}
+
+let make_tier_1 () = 
+	let one = make_card White 0
+	{red = 0;
+	blue = 3;
+	black = 0;
+	green = 0;
+	white = 0} in
+	let two = make_card White 0 
+	{red = 2;
+	blue = 0;
+	black = 1;
+	green = 0;
+	white = 0} in
+	let three = make_card White 0 
+	{red = 1;
+	blue = 1;
+	black = 1;
+	green = 1;
+	white = 0} in
+	let four = make_card White 0 
+	{red = 0;
+	blue = 2;
+	black = 2;
+	green = 0;
+	white = 0} in
+	let five = make_card White 1 
+	{red = 0;
+	blue = 0;
+	black = 0;
+	green = 4;
+	white = 0} in
+	let six = make_card White 0 
+	{red = 1;
+	blue = 1;
+	black = 1;
+	green = 2;
+	white = 0} in
+	let seven = make_card White 0 
+	{red = 0;
+	blue = 2;
+	black = 1;
+	green = 2;
+	white = 0} in
+	let eight = make_card White 0 
+	{red = 0;
+	blue = 1;
+	black = 1;
+	green = 0;
+	white = 3} in
+	let nine = make_card Blue 0 
+	{red = 0;
+	blue = 0;
+	black = 2;
+	green = 0;
+	white = 1} in
+	let ten = make_card Blue 0 
+	{red = 0;
+	blue = 0;
+	black = 3;
+	green = 0;
+	white = 0} in
+	let eleven = make_card Blue 0 
+	{red = 1;
+	blue = 0;
+	black = 1;
+	green = 1;
+	white = 1} in
+	let twelve = make_card Blue 0 
+	{red = 0;
+	blue = 0;
+	black = 2;
+	green = 2;
+	white = 0} in
+	let thirteen = make_card Blue 0 
+	{red = 4;
+	blue = 0;
+	black = 0;
+	green = 0;
+	white = 0} in
+
 
 let init_state starting_player num_human num_ai = 
 	failwith "Unimplemented"
