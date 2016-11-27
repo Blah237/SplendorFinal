1dc8da5525957fcc5693ded63438526c19cede19
Sean Viswanathan <Sean@Seans-MacBook-Pro-2.local>
2016-11-26 21:32:34 -0500

    buy card, need to handle the buy when gold is involved

--- play.ml
+++ play.ml
@@ -891,7 +891,7 @@ let buy_card p s c =
 		 if exists c s.tier1 then
 		   let (deck,table) = refresh_cards s.tier1_deck tier1 c in
 			 let new_p_list = List.tl s.players @ [updated_player] in
-			 {
+			 Some {
 				 players = new_p_list;
 					 (** a list of the players playing the game *)
 				 tier1_deck = deck
@@ -915,7 +915,57 @@ let buy_card p s c =
 				 (** the number of gold coins available **)
 			 }
 		 else if exists c s.tier2 then
+			 let (deck,table) = refresh_cards s.tier2_deck tier2 c in
+			 let new_p_list = List.tl s.players @ [updated_player] in
+			 Some {
+				 players = new_p_list;
+					 (** a list of the players playing the game *)
+				 tier1_deck = s.tier1_deck
+				 tier2_deck = deck
+				 tier3_deck = s.tier3_deck
+					 (** the cards remaining in the decks of cards, seperated by tier *)
+				 tier1 = s.tier1
+				 tier2 = table
+				 tier3 = s.tier3
+					 (** the cards available, seperated by tier *)
+				 nobles = s.nobles
+					 (** the nobles currently available *)
+				 available_gems = table_gems
+					 (** the gems currently available for taking *)
+				 gem_piles = calc_gem_piles s.gem_piles
+				 (** used for checking how many gems a player can/must take if there are
+				 less than three piles *)
+				 turns_taken = s.turns_taken + 1
+				 (** used for ai behavior *)
+				 gold = s.gold
+				 (** the number of gold coins available **)
+			 }
 		 else
+			 let (deck,table) = refresh_cards s.tier3_deck tier3 c in
+			 let new_p_list = List.tl s.players @ [updated_player] in
+			 Some {
+				 players = new_p_list;
+					 (** a list of the players playing the game *)
+				 tier1_deck = s.tier1_deck
+				 tier2_deck = s.tier2_deck
+				 tier3_deck = deck
+					 (** the cards remaining in the decks of cards, seperated by tier *)
+				 tier1 = s.tier1
+				 tier2 = s.tier2
+				 tier3 = table
+					 (** the cards available, seperated by tier *)
+				 nobles = s.nobles
+					 (** the nobles currently available *)
+				 available_gems = table_gems
+					 (** the gems currently available for taking *)
+				 gem_piles = calc_gem_piles s.gem_piles
+				 (** used for checking how many gems a player can/must take if there are
+				 less than three piles *)
+				 turns_taken = s.turns_taken + 1
+				 (** used for ai behavior *)
+				 gold = s.gold
+				 (** the number of gold coins available **)
+			 }
 	| (true,x) ->
 	| _ -> None
 
