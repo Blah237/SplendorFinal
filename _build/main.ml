fb41660509252913eea3eb09844e16f6bc01c87a
Nathaniel Kaplan <nak74@cornell.edu>
2016-11-27 01:00:20 -0500

    ai early game

--- _build/main.ml
+++
@@ -1,8 +0,0 @@
-open Play
-open Card
-open Ai
-
-(** This is the main function that calls the necessary functions
-to run the game *)
-let main () =
-	failwith "Unimplemented"
--- _build/main.mli
+++
@@ -1,7 +0,0 @@
-open Play
-open Card
-open Ai
-
-(** This is the main function that calls the necessary functions
-to run the game *)
-val main : unit -> unit
