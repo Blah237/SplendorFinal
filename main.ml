7c13147f3c8f766628d70238b830159c6e20f047
Nathaniel Kaplan <nak74@cornell.edu>
2016-12-05 00:40:39 -0500

    merge

--- main.ml
+++ main.ml
@@ -5,5 +5,5 @@ open Graphic
 
 (** This is the main function that calls the necessary functions
 to run the game *)
-let () =
-	let start_state = init_state 1 1 in repl start_state ""
+let main () =
+	failwith "Unimplemented"
